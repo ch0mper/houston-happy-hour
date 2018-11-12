964f88327d98b914b4ff598258621950d8bd6161
ch0mper <hannahchang17@gmail.com>
2018-11-12 15:51:17 -0600

    added bars new bars edit and updated seed file

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,9 +1,31 @@
 class BarsController < ApplicationController
+  before_action :define_current_bars
+
   def index
     @bars = Bar.all
   end
 
   def show
-    @bar = Bar.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @bar = Bar.create(bar_params)
+    redirect_to @bar
+  end
+
+  def define_current_bars
+    if params[:id]
+      @bar = Bar.find(params[:id])
+    else
+      @bar = Bar.new
+    end
+  end
+
+  def bar_params
+    params.require(:bar).permit(:name, :address, :phone_number, :neighborhood, :vibe, :price_level, :sake, :beer, :cocktail, :beer_and_shot, :wine_glass, :wine_bottle, :start_time, :end_time)
+  end
+
 end
