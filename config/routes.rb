705ab23db89a8734d57fbf03b91e5a18a999ff0f
github username <github email address>
2018-11-13 12:08:13 -0600

    logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+
+  resources :secrets
+  get 'secrets/show'
+  root to: 'sessions#new'
   resources :bars
   resources :users
   resources :user_bars
+  resources :sessions, only: [:create]
+  get '/sessions', to: 'sessions#new'
+  delete '/sessions', to: 'sessions#destroy', as: 'logout'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
